409284dd0cae8a6c8ef2de82517909e6b7e750b9
Abbos Bobomurodov <abbos2101@gmail.com>
2021-12-14 08:07:55 +0500

    Initial commit

---
+++ lib/pages/teledart_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:teledart/model.dart';
+import 'package:teledart/teledart.dart';
+import 'package:teledart/telegram.dart';
+
+class TeleDartPage extends StatefulWidget {
+  const TeleDartPage({Key? key}) : super(key: key);
+
+  @override
+  _TeleDartPageState createState() => _TeleDartPageState();
+}
+
+class _TeleDartPageState extends State<TeleDartPage> {
+  late TeleDart teledart;
+
+  @override
+  void initState() {
+    loadData();
+    super.initState();
+  }
+
+  void loadData() async {
+    final telegram = Telegram("1139237420:AAGxE6lYgLwLKPODCSEsXa8KN3W5TFvZhvs");
+    final event = Event((await telegram.getMe()).username!);
+    teledart = TeleDart(telegram, event);
+    teledart.start();
+  }
+
+  @override
+  void dispose() {
+    teledart.stop();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Teledart")),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          Message event = Message(
+            date: DateTime.now().millisecond +
+                const Duration(hours: 3).inMilliseconds,
+            chat: Chat(
+              id: 468051408,
+              type: 'private',
+            ),
+            message_id: 1471,
+          );
+          // teledart.onMessage(keyword: "start").listen((event) {
+          //   teledart.replyMessage(event, "text");
+          //   print(event.toJson());
+          // });
+        },
+      ),
+    );
+  }
+}
